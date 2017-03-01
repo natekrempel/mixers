7696ea9fe3210762169bcf2885f360224040439c
Nate Krempel <nate@wcst.com>
2017-03-01 11:22:50 -0600

    testing fs-extra

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 #! /usr/bin/env node
-'use strict';
+let userArgs = process.argv.slice(2);
+let destination = userArgs[0];
+let styleDirectory = 'Mixers';
 
-var userArgs = process.argv.slice(2);
-var destination = userArgs[0];
-var styleDirectory = 'Mixers';
+const fs = require('fs-extra');
+const path = require('path');
+const spawn = require('child_process').spawn;
 
-var path = require('path');
-var spawn = require('child_process').spawn;
+const getCurrentDir = () => {
+  const currentDir = spawn('/bin/pwd');
 
-var getCurrentDir = function getCurrentDir() {
-  var currentDir = spawn('/bin/pwd');
-
-  currentDir.stdout.on('data', function (data) {
+  currentDir.stdout.on('data', (data) => {
     copy(data.toString().replace(/[\n\r]/g, ''));
   });
 
-  currentDir.on('error', function (code) {
-    console.log('Error code: ' + code);
+  currentDir.on('error', (code) => {
+    console.log(`Error code: ${code}`);
   });
 };
 
-var copy = function copy(dest) {
-  var copyDir = spawn('/bin/cp', ['-r', __dirname + '/stylesheets', dest + '/' + styleDirectory]);
+// const confirmCopy = () => {
+//   console.log('confirming files copied');
+// };
 
-  copyDir.stdout.on('data', function (data) {
-    console.log('Success! Mixers have been placed in ' + dest + '/' + styleDirectory);
+const copy = (dest) => {
+  fs.copy(`${__dirname}/stylesheets`, `${dest}/${styleDirectory}`, err => {
+    if (err) return console.error(err);
+    console.log('success!');
   });
-
-  copyDir.on('error', function (code) {
-    console.log('Error code: ' + code);
-  });
+  // console.log(`Placing files in ${dest}/${styleDirectory}`);
+  // const copyDir = spawn('/bin/cp', ['-r', `${__dirname}/stylesheets`, `${dest}/${styleDirectory}`]);
+  //
+  // copyDir.stdout.on('data', (data) => {
+  //   // console.log(`Success! Mixers have been placed in ${dest}/${styleDirectory}`);
+  // });
+  //
+  // copyDir.stderr.on('data', (data) => {
+  //   console.log(`stderr: ${data}`);
+  // });
+  //
+  // copyDir.on('close', (code) => {
+  //   console.log(`child process exited with code ${code}`);
+  //   confirmCopy();
+  //   // Test to confirm files are where they should be
+  //   // Return a success message if they are
+  // });
 };
 
 if (destination) {
