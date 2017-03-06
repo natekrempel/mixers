2bb76ec27dcf27569d94a9485a8783fcdbcf6955
Nate Krempel <nate@wcst.com>
2017-03-06 15:41:00 -0600

    simplified proccess and added better command / flag handling

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #! /usr/bin/env node
-let userArgs = process.argv.slice(2);
-let destination = userArgs[0];
+
 let styleDirectory = 'Mixers';
 
-const fs = require('fs-extra');
+const fse = require('fs-extra');
 const path = require('path');
 const spawn = require('child_process').spawn;
+var args = require('minimist')(process.argv.slice(2));
+
+// TODO: Commands
+// update (updates sass stylesheets)
 
 const getCurrentDir = () => {
   const currentDir = spawn('/bin/pwd');
@@ -19,36 +22,30 @@ const getCurrentDir = () => {
   });
 };
 
-// const confirmCopy = () => {
-//   console.log('confirming files copied');
-// };
-
-const copy = (dest) => {
-  fs.copy(`${__dirname}/stylesheets`, `${dest}/${styleDirectory}`, err => {
-    if (err) return console.error(err);
-    console.log('success!');
-  });
-  // console.log(`Placing files in ${dest}/${styleDirectory}`);
-  // const copyDir = spawn('/bin/cp', ['-r', `${__dirname}/stylesheets`, `${dest}/${styleDirectory}`]);
-  //
-  // copyDir.stdout.on('data', (data) => {
-  //   // console.log(`Success! Mixers have been placed in ${dest}/${styleDirectory}`);
-  // });
-  //
-  // copyDir.stderr.on('data', (data) => {
-  //   console.log(`stderr: ${data}`);
-  // });
-  //
-  // copyDir.on('close', (code) => {
-  //   console.log(`child process exited with code ${code}`);
-  //   confirmCopy();
-  //   // Test to confirm files are where they should be
-  //   // Return a success message if they are
-  // });
+const confirmCopy = (dest) => {
+  let testForFiles = fse.existsSync(`${dest}/${styleDirectory}`);
+  if (testForFiles) {
+    console.log(`Files successfully placed in ${dest}`);
+  } else {
+    console.log(`An error occured placing the files in ${dest}`);
+  }
 };
 
-if (destination) {
-  copy(path.normalize(destination));
-} else {
+const copy = (dest) => {
+  fse.copy(`${__dirname}/stylesheets`, `${dest}/${styleDirectory}`, err => {
+    if (err) return console.error(err);
+    confirmCopy(dest);
+  });
+};
+
+if (args._.includes('install') && args.dest) {
+  console.log(`Installing styles in ${args.dest}`);
+  copy(path.normalize(args.dest));
+} else if (args._.includes('install')) {
+  console.log('Installing styles in the current directory');
   getCurrentDir();
+} else if (args._.includes('update')) {
+  console.log('Update sass directory, once updated do an install');
+} else {
+  console.error('Command not found');
 }
