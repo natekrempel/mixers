84df15076bbcc92ac6dd266da53efb418473bb40
Nate Krempel <nate@wcst.com>
2018-02-22 22:15:56 -0600

    Added comments

--- index.js
+++ index.js
@@ -2,26 +2,37 @@
 
 let styleDirectory = 'Mixers';
 
+// fse adds file system methods that aren't included in the native fs module ()
 const fse = require('fs-extra');
+// Node modules used for working with file and directory paths
 const path = require('path');
+// used to spawn a child process that allows us to run a Shell command in Node
 const spawn = require('child_process').spawn;
+// used to grab arguments entered in the commandline
 var args = require('minimist')(process.argv.slice(2));
 
 // TODO: Commands
 // update (updates sass stylesheets)
 
+// Get the path to the directory the user is cd-ed into via the commandline
 const getCurrentDir = () => {
+  // uses spawn to run the command `pwd` to get the current directory. Using /bin/pwd to ensure the the actual working directory since just `pwd` can be different if a symlink is used.
   const currentDir = spawn('/bin/pwd');
 
+  // Add a listener to the output of the `currentDir` child_process command.
+  // stdout gets us the result of the `/bin/pwd` command
   currentDir.stdout.on('data', (data) => {
+    // Clean up the returned directory path and pass it into the copy function
     copy(data.toString().replace(/[\n\r]/g, ''));
   });
 
+  // Add a listener if there is an error
   currentDir.on('error', (code) => {
     console.log(`Error code: ${code}`);
   });
 };
 
+// Confirm that the mixin directory has been copied to the proper directory
 const confirmCopy = (dest) => {
   let testForFiles = fse.existsSync(`${dest}/${styleDirectory}`);
   if (testForFiles) {
@@ -31,6 +42,7 @@ const confirmCopy = (dest) => {
   }
 };
 
+// Copy the mixins directory to the specified path
 const copy = (dest) => {
   fse.copy(`${__dirname}/stylesheets`, `${dest}/${styleDirectory}`, err => {
     if (err) return console.error(err);
